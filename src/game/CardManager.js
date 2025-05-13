6cddc5a92de7a0448833c3d5af99325f55147f71
mmoundraty <mmoundraty@ucsd.edu>
2025-05-12 22:57:41 -0700

    added export keyword and import for the test

--- src/game/CardManager.js
+++ src/game/CardManager.js
@@ -1,23 +1,22 @@
 
 
-function shuffle(card) {
+export function shuffle(card) {
   // Fisher-Yates shuffle algorithm
   
 }
 
-function draw(cards, number) {
+export function draw(cards, number) {
     // Draw the specified number of cards from the deck
     //Should call Deck.draw() for a card then Hand.add() to the hand
     
 }
 
-function moveCard(card, fromCollection, toCollection) {
+export function moveCard(card, fromCollection, toCollection) {
     // Moving cards between deck, hand, discard, etc.
-
     
 }
 
-function filterCards(cards, condition) {
+export function filterCards(cards, condition) {
     // Filter cards based on a condition (e.g., type, rarity, etc.)
-    
+
 }
--- tests/CardManager.test.js
+++ tests/CardManager.test.js
@@ -1,6 +1,12 @@
+import {shuffle, draw, moveCard, filterCards} from '../src/game/CardManager.js' 
+
+/*
 testExample('shuffle', () => {
     let deck = ['Ace', '2', '3', '4', '5'];
     shuffle(deck);
 
     expect(cards.sort()).toEqual(original.sort());
 });
+*/
+
+
