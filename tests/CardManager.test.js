ab093242b89d5b60d61a6782cb215fb257be1d80
Victoria Timofeev <victoria.ti@yahoo.com>
2025-05-13 13:30:16 -0700

    Update CardManager.test.js
    Added filterCards unit test

--- tests/CardManager.test.js
+++ tests/CardManager.test.js
@@ -9,4 +9,18 @@ testExample('shuffle', () => {
 });
 */
 
+//filterCards function test
+test('filterCards filters by cost', () => {
+    const cards = [
+        { name: 'Free Boba', type: 'special', cost: 1 },
+        { name: 'Early Submission', type: 'special', cost: 0 }
+    ];
+
+    const result = filterCards(cards, card => card.cost === 0);
+
+    expect(result).toEqual([
+        { name: 'Early Submission', type: 'special', cost: 0 }
+      ]);
+});
+
 
