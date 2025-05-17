3923980aac36ebc04b9f2e34d632486f03151d4d
faker1897 <yangyunsong040529@163.com>
2025-05-17 15:56:25 -0700

    fix image path of the card

--- src/components/cards/SevereCrashOut.js
+++ src/components/cards/SevereCrashOut.js
@@ -2,14 +2,14 @@ import Card from "../Card.js";
 
 class SevereCrashOut extends Card {
     constructor() {
-        const src = new URL('../../img/SevereCrashOut.png', import.meta.url).href;
+
         super({
             name: "Severe Crash Out",
             type: "attack",
             cost: 1,
             description: "You spent 6 hours trying to automate something which would have taken 6 minutes to do by hand and now you’re genuinely considering switching your major.",
             effect: "Deal 5 damage.",
-            image: src,
+            image: "/src/img/SevereCrashOut.png",
         });
     }
     /**
