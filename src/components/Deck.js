ff12a34fc620c59d292e0edda5927a8c4ae06021
mmoundraty <mmoundraty@ucsd.edu>
2025-05-19 16:10:00 -0700

    Added Deck branch

---
+++ src/components/Deck.js
@@ -0,0 +1,25 @@
+
+class Deck extends HTMLElement {
+    constructor() {
+        this.deck = [];
+    }
+
+    addCard(card) {
+        //Adds a card to the deck
+
+    }
+
+    removeCard(card) {
+        //Removes a card from the deck
+
+    }
+
+    drawCard() {
+        //Pops and returns a card form the deck
+
+    }
+
+    renderDeckHelper() {
+        //Helper function to help render cards
+    }
+}
