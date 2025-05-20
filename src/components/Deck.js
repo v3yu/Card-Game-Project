b29687dd2b15bcd9997c7adb96dcc9354fcbf721
BOTxJOHN <rouzin.evgeny@gmail.com>
2025-05-19 20:15:45 -0700

    Deck.js + Deck.test.js

--- src/components/Deck.js
+++ src/components/Deck.js
@@ -1,25 +1,75 @@
+import {Card} from './Card.js';
 
 class Deck extends HTMLElement {
+    
+    #deck;
+    
     constructor() {
-        this.deck = [];
+        super();
+        this.#deck = [];
     }
 
+    /**
+     * @param {Card} card - card that needs to be added
+     * @returns {number} - -1 if card is not a card object, 0 on success
+     */
     addCard(card) {
-        //Adds a card to the deck
-
+        if (!(card instanceof Card)) return -1;
+        this.#deck.push(card);
+        return 0;
     }
 
+    /**
+     * @param {Card} card - card that needs to be removed
+     * @return {number} - index of the card that was removed, or -1 if card wasn't found (or card is not a card object)
+     */
     removeCard(card) {
-        //Removes a card from the deck
-
+        if (!(card instanceof Card)) return -1;
+        let index = this.#deck.indexOf(card);
+        this.#deck.splice(index, index+1);
+        return index; //returns index of deleted card or -1 if no such card was found
     }
 
+    /**
+     * 
+     * @returns {Card} - drawed card
+     */
     drawCard() {
-        //Pops and returns a card form the deck
-
+        let drawedCard = this.#deck.pop();
+        return drawedCard;
     }
 
-    renderDeckHelper() {
-        //Helper function to help render cards
+    
+    /**
+     * 
+     * @returns {number} - number of cards in the deck
+     */
+    deckSize() {
+        return this.#deck.length;
+    }
+
+    /**
+     * function to see the whole deck
+     * @returns {Card[]} - current deck
+     */
+    showDeck() {
+        return this.#deck;
+    }
+
+    /**
+     * 
+     * @param {Card} card - card to be rendered
+     * @return {number} - -1 if card is not a card object, 0 on success
+     */
+    renderCard(card) {
+        if (!(card instanceof Card)) return -1;
+        const body = document.querySelector('body');
+        card.render();
+        body.append(card);
+        return 0;
     }
 }
+
+customElements.define('player-deck', Deck);
+
+export default Deck;
