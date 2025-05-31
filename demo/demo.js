6c6960224517612121b56f7d98c2363a9aecf12c
Vivian Yu <146399020+v3yu@users.noreply.github.com>
2025-05-31 23:25:11 +0000

    demonstrate dynamic rendering of a hand of cards in the battle scene

--- demo/demo.js
+++ demo/demo.js
@@ -1,9 +1,21 @@
 import Hand from "../src/components/Hand.js";
 import {CrashOut} from "../src/components/cards/CrashOut.js";
+import {FreeBoba} from "../src/components/cards/FreeBoba.js"
 
 const body = document.querySelector('body')
+const handArea = document.querySelector('.hand-area');
 
 const hand = new Hand();
-body.append(hand)
-hand.addCard(new CrashOut())
-hand.renderHandHelper()
+handArea.append(hand)
+const crashOutCard = new CrashOut();
+const freeBobaCard = new FreeBoba();
+const crashOutCard2 = new CrashOut();
+
+hand.addCard(crashOutCard);
+hand.addCard(crashOutCard2);
+hand.addCard(freeBobaCard);
+
+//remove
+//hand.removeCard(freeBobaCard);
+
+hand.renderHandHelper();
