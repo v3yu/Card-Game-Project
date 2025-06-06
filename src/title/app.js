ce3bfa0a68aa2cc3dd5ec8ae2bd375eb28303555
raeedbourai <rbourai04@gmail.com>
2025-06-06 15:46:09 -0700

    Started basic actions on volume control + play

--- src/title/app.js
+++ src/title/app.js
@@ -1,1 +1,31 @@
+document.getElementById("play").addEventListener("click", loadStory);
 
+function loadStory() {
+  location='https://example.com';
+}
+
+let vol = document.getElementById('volume-controls');
+
+function showVolume(){
+    vol.style.display = 'grid';
+}
+function hideVolume(){
+    vol.style.display = 'none';
+}
+
+function toggleVolume(){
+    if(vol.style.display === 'grid'){
+        hideVolume();
+    }else{
+        showVolume();
+    }
+}
+
+function dimScreen(){
+    document.getElementById('overlay').style.display = 'block';
+}
+
+document.getElementById('settings').addEventListener('click', () => {
+    toggleVolume();
+    dimScreen();
+});
