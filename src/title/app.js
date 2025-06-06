031a6da1a9efe7359b89e65cfeef59a963fff099
raeedbourai <rbourai04@gmail.com>
2025-06-06 16:59:56 -0700

    Worked on centered volume and dim

--- src/title/app.js
+++ src/title/app.js
@@ -13,30 +13,34 @@ function hideVolume(){
     vol.style.display = 'none';
 }
 
-function toggleVolume(){
-    if(vol.style.display === 'grid'){
-        hideVolume();
-    }else{
-        showVolume();
-    }
+let settings = document.getElementsByClassName('volSettings')[0];
+
+function showSettings(){
+    settings.style.display = 'block';
+}
+
+function hideSettings(){
+    settings.style.display = 'none';
 }
 
 function dimScreen(){
     document.getElementById('overlay').style.display = 'block';
     document.getElementById('exit').style.display = 'block';
+    showVolume();
+    showSettings();
 }
 
 function unDimScreen(){
     document.getElementById('overlay').style.display = 'none';
     document.getElementById('exit').style.display = 'none';
+    hideVolume();
+    hideSettings();
 }
 
 document.getElementById('settings').addEventListener('click', () => {
     dimScreen();
-    toggleVolume();
 });
 
 document.getElementById('exit').addEventListener('click', () => {
     unDimScreen();
-    toggleVolume();
 })
