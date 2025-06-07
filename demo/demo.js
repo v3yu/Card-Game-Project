2d7e77cd3d5f4a9b10b2ffd9a5f2f5d248b7c0d7
faker1897 <yangyunsong040529@163.com>
2025-06-07 16:57:30 -0700

    merge the battle scene and discard-visualize

--- demo/demo.js
+++ demo/demo.js
@@ -1,7 +1,6 @@
 
 
 //Test hand
-
 import {CrashOut} from '../src/components/cards/CrashOut.js';
 import Hand from '../src/components/Hand.js';
 import Discard from '../src/components/Discard.js';
@@ -14,7 +13,7 @@ import {Battle} from '../src/game/Battle.js';
 
 
 
-const deck = new Deck([new CrashOut(),new CrashOut(),new CrashOut(),new CrashOut(),new CrashOut(),new CrashOut(),new CrashOut(),new CrashOut()]);
+const deck = new Deck([new CrashOut(),new CrashOut(),new CrashOut(),new CrashOut(),new CrashOut(),new CrashOut(),new CrashOut(),new CrashOut(),new CrashOut(),new CrashOut(),new CrashOut(),new CrashOut(),new CrashOut(),new CrashOut(),new CrashOut(),new CrashOut(),new CrashOut()]);
 
 console.log(deck.getCards());
 const player = new Player(10000, 3,deck,new Hand(),new Discard());
