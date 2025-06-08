3acb00c3f9efa72e3c8b2338e407e11c17d946fb
faker1897 <yangyunsong040529@163.com>
2025-06-07 18:13:39 -0700

    fix lint error

--- src/cutscene1/app.js
+++ src/cutscene1/app.js
@@ -3,21 +3,29 @@ let img2 = document.getElementById('cutscene2-bg');
 
 
 
+/**
+ *
+ */
 function loadImg2() {
   img1.style.display = 'none';
   img2.style.display = 'block';
 }
 
-img1.addEventListener("click", loadImg2);
+img1.addEventListener('click', loadImg2);
 
 
+/**
+ *
+ */
 function loadNextChapter() {
-  console.log("loadNextChapter triggered");
+  console.log('loadNextChapter triggered');
   document.getElementById('transition').style.opacity = 1;
 	setTimeout(() => {
-  	console.log("Delayed for 3 second.");
-		location.href="../cutscene2/index.html";
+
+
+  console.log('Delayed for 3 second.');
+  location.href='../cutscene2/index.html';
 	}, 3000);
 }
 
-img2.addEventListener("click", loadNextChapter);
+img2.addEventListener('click', loadNextChapter);
--- src/cutscene2/app.js
+++ src/cutscene2/app.js
@@ -4,28 +4,37 @@ let img3 = document.getElementById('cutscene3-bg');
 
 
 
+/**
+ *
+ */
 function loadImg2() {
   img1.style.display = 'none';
   img2.style.display = 'block';
 }
 
-img1.addEventListener("click", loadImg2);
+img1.addEventListener('click', loadImg2);
 
+/**
+ *
+ */
 function loadImg3() {
   img2.style.display = 'none';
   img3.style.display = 'block';
 }
 
-img2.addEventListener("click", loadImg3);
+img2.addEventListener('click', loadImg3);
 
 
+/**
+ *
+ */
 function loadBattle() {
-  console.log("loadBattle triggered");
+  console.log('loadBattle triggered');
   document.getElementById('transition').style.opacity = 1;
 	setTimeout(() => {
-  	console.log("Delayed for 3 second.");
-		location.href="../../demo/cardtest.html";
+  console.log('Delayed for 3 second.');
+  location.href='../../demo/cardtest.html';
 	}, 3000);
 }
 
-img3.addEventListener("click", loadBattle);
+img3.addEventListener('click', loadBattle);
