25c50f0517336fbac7edc559d66ca358017b9609
raeedbourai <rbourai04@gmail.com>
2025-06-07 17:48:20 -0700

    link to demo test to try it out

--- src/cutscene2/app.js
+++ src/cutscene2/app.js
@@ -24,7 +24,7 @@ function loadBattle() {
   document.getElementById('transition').style.opacity = 1;
 	setTimeout(() => {
   	console.log("Delayed for 3 second.");
-		//location.href="../cutscene2/index.html";
+		location.href="demo/cardtest.html";
 	}, 3000);
 }
 
